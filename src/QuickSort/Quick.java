df2aa0201bf837fb5db7311312d9116e0d31a359
AshishJha236 <jhaa33716@gmail.com>
2024-10-01 19:37:48 +0530

    Quick Sort Changes

--- src/QuickSort/Quick.java
+++ src/QuickSort/Quick.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Quick {
     public static void main(String[] args) {
-        int[] arr = {5, 4, 3, 2, 1};
+        int[] arr = {6, 5, 4, 3, 2, 1};
         sort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
 //        Arrays.sort(arr);
